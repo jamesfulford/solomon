df30b9c9febd79f6fad14f152aed93ca52eb2a3e
James Fulford <james.fulford@outlook.com>
2020-12-25 15:14:59 -0500

    WIP: tagging

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+export TAG=`git rev-parse HEAD`
+
+# build and push containers
+docker context use default
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 383314943195.dkr.ecr.us-east-1.amazonaws.com
+docker-compose build && docker-compose push
+
+# update database
+./seed-database.sh
+
+# update containers
+docker context use solomon
+docker compose up
+
+docker context use default
+
+# tagging
+git tag -a prod -m "prod deployed `date`"
+git push tag
