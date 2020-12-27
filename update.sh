23386a3e80a20982b5796a260159383387e70d7d
James Fulford <james.fulford@outlook.com>
2020-12-26 22:44:10 -0500

    feat(update): scripting updates

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 export TAG=`git rev-parse HEAD`
 
+# await prior updates to settle
+aws cloudformation wait stack-update-complete --stack-name solomon
+
 # build and push containers
 docker context use default
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 383314943195.dkr.ecr.us-east-1.amazonaws.com
@@ -10,13 +13,34 @@ docker-compose build && docker-compose push
 # update database
 ./seed-database.sh
 
-# update containers
+# switch contexts
+trap "docker context use default" EXIT
 docker context use solomon
-docker compose up
 
-docker context use default
+# execute update
+set +e
+output=`docker compose up 2>&1`
+return_code="$?"
 
-# tagging
+echo $output
+
+echo
+
+# if error was because there were no changes, then continue anyway
+if [[ "0" != "$return_code" ]]; then
+    if [[ $output == *"The submitted information didn't contain changes. Submit different information to create a change set."* ]]; then
+        echo "(Its fine, just no changes is all.)"
+        echo
+    else
+        echo "Some error occurred. Exiting!"
+        exit 1
+    fi
+fi
+
+set -e
+
+
+# tag commit currently deployed in prod
 git tag -d prod
 git push origin :prod
 git tag -a prod -m "prod deployed `date`"
