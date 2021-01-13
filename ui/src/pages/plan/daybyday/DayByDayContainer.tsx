6560221a40ba7dda383b2d887c750ddef3b9d6d6
James Fulford <james.fulford@outlook.com>
2021-01-12 19:19:03 -0500

    refactor(ui): separate out duration selector

--- ui/src/pages/plan/daybyday/DayByDayContainer.tsx
+++ ui/src/pages/plan/daybyday/DayByDayContainer.tsx
@@ -1,12 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import Chart from "react-google-charts";
 import Container from 'react-bootstrap/Container';
 import { getFlags } from '../../../store/reducers/flags/getters';
 import { useSelector } from 'react-redux';
 import { getDayByDay } from '../../../store/reducers/daybydays/getters';
 import { getParameters } from '../../../store/reducers/parameters/getters';
-import { useThunkDispatch } from '../../../useDispatch';
-import { setParameters } from '../../../store/reducers/parameters';
+import { DurationSelector } from '../DurationSelector';
 
 
 export interface IApiDayByDay {
@@ -125,34 +124,18 @@ const DayByDayChart = ({ daybyday, chartType, setAside }: { daybyday: IApiDayByD
 }
 
 
-function getComputedDurationDays(startDate: string, minimumEndDate: string): number | undefined {
-    if (minimumEndDate) {
-        const start = new Date(startDate);
-        const computedEndDate = new Date(minimumEndDate);
-        return Math.round((computedEndDate.getTime() - start.getTime()) / (1000 * 60 * 60 * 24))
-    }
-}
 
 export const DayByDayContainer = () => {
     const {
         flags: { highLowEnabled },
         daybydays: { data, loading, error },
-        parameters: { startDate, setAside, }
+        parameters: { setAside, }
     } = useSelector(state => ({
         flags: getFlags(state as any),
         daybydays: getDayByDay(state as any),
         parameters: getParameters(state as any).data,
     }));
 
-    const dispatch = useThunkDispatch();
-
-    const setQueryRangeDays = useCallback((computedDurationDays: number) => {
-        const start = new Date(startDate);
-        const durationMs = 1000 * 60 * 60 * 24 * computedDurationDays;
-        dispatch(setParameters({
-            endDate: new Date(start.getTime() + durationMs).toISOString().split('T')[0]
-        }) as any);
-    }, [startDate, dispatch])
 
     const [chartType, setChartType] = useState<ChartTab>(ChartTab.DISPOSABLE_INCOME);
 
@@ -185,8 +168,6 @@ export const DayByDayContainer = () => {
         tabs.push(ChartTab.UNCERTAINTY);
     }
 
-    const computedDurationDays = getComputedDurationDays(startDate, daybyday.params.minimumEndDate);
-
     return <>
         <ul className="nav nav-tabs">
             {tabs.map(chart => <li className="nav-item" key={chart}>
@@ -199,28 +180,7 @@ export const DayByDayContainer = () => {
                 </button>
             </li>)}
         </ul>
-        <DayByDayChart chartType={chartType} daybyday={daybyday} setAside={setAside} /> 
-        <div className="text-center">
-            <button className="btn btn-outline-primary btn-sm mr-1" onClick={() => {setQueryRangeDays(Math.min(90, computedDurationDays || 90))}}>Default</button>
-            {[
-                { days: 365, display: '1y', danger: false },
-                { days: 365 * 2, display: '2y', danger: false },
-                { days: 365 * 5, display: '5y', danger: true },
-                { days: 365 * 10, display: '10y', danger: true },
-                { days: 365 * 20, display: '20y', danger: true },
-                { days: 365 * 30, display: '30y', danger: true },
-            ]
-                .filter(({ days }) => {
-                    if (!computedDurationDays) {
-                        return true;
-                    }
-                    return days > computedDurationDays;
-                })
-                .map(({ days, display, danger }) => {
-                    return <button key={days} className={`btn ${danger ? 'btn-outline-danger' : 'btn-outline-primary'} btn-sm mr-1`} onClick={() => {setQueryRangeDays(days)}}>{display}</button>
-                })
-            }
-            <br />
-        </div>
+        <DayByDayChart chartType={chartType} daybyday={daybyday} setAside={setAside} />
+        <DurationSelector />
     </>
 }
