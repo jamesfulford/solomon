fbf70c803a2fd081a5e99969f084d99b5c6afa84
James Fulford <james.fulford@outlook.com>
2021-01-13 22:45:25 -0500

    fix(ui): proper plugin usage

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -3,12 +3,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 const storeCreator = () => {
+    const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
     const store = createStore(
       rootReducer,
-      compose(
-        applyMiddleware(thunk),
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      {},
+      composeEnhancers(
+        applyMiddleware(thunk)
       )
     );
 
