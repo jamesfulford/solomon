0597fde6f7711a8bac260149b40991d65b46d237
James Fulford <james.fulford@outlook.com>
2021-01-24 18:50:38 -0500

    WIP

--- update-droplet.sh
+++ update-droplet.sh
@@ -9,7 +9,7 @@ pip3 install --upgrade --user awscli
 
 # ECR is where images are pushed to by CI
 # `aws configure` was configured manually once
-./.local/bin/aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 383314943195.dkr.ecr.us-east-1.amazonaws.com
+~/.local/bin/aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 383314943195.dkr.ecr.us-east-1.amazonaws.com
 
 # a pull should have been done earlier
 docker-compose pull
